148b628d8492f2fb23cef7d62137faf4ab42f39e
milian-rm <rmilian-2021493@kinal.edu.gt>
2025-09-07 22:18:22 -0600

    T4/ Archivos Iniciales para el Proyecto

---
+++ src/main/java/org/flim/bufeteflim/BufeteflimApplication.java
@@ -0,0 +1,13 @@
+package org.flim.bufeteflim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BufeteflimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BufeteflimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/flim/bufeteflim/BufeteflimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flim.bufeteflim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BufeteflimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
