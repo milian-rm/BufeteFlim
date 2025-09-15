9ab18386be6aa8ddcf0938dbae11701e7c8a8450
Lendrock <lchoxon-2021286@kinal.edu.gt>
2025-09-14 18:31:39 -0600

    T18/Se creó el Controller para la entidad Casos

---
+++ src/main/java/org/flim/bufeteflim/web/controller/CasoController.java
@@ -0,0 +1,68 @@
+package org.flim.bufeteflim.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.flim.bufeteflim.dominio.dto.CasoDto;
+import org.flim.bufeteflim.dominio.dto.ModCasoDto;
+import org.flim.bufeteflim.dominio.service.CasoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/casos")
+@Tag(name = "Casos", description = "Operaciones Crud para Casos dentro de Bufete Flim")
+public class CasoController {
+    private final CasoService casoService;
+
+    public CasoController(CasoService casoService){
+        this.casoService = casoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CasoDto>> obtenerCasos(){
+        return ResponseEntity.ok(casoService.obtenerTodo());
+    }
+
+    @GetMapping("/{idCaso}")
+    @Operation(
+            summary = "Obtener un Caso por su identificador",
+            description = "Retorna la parte que coincida con el identificador enviado",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Caso encontrado"),
+                    @ApiResponse(responseCode = "404", description = "Caso no encontrado", content = @Content)
+            }
+    )
+    public ResponseEntity<CasoDto> buscarPorId
+            (@Parameter(description = "Identificador del Caso a recuperar", example = "1")
+             @PathVariable Long idCaso){
+        return ResponseEntity.ok(this.casoService.buscarPorId(idCaso));
+    }
+
+    //Crear - agregar
+    @PostMapping
+    public ResponseEntity<CasoDto> guardarCaso
+    (@RequestBody @Valid CasoDto casoDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.casoService.guardarCaso(casoDto));
+    }
+
+    //modificar
+    @PutMapping("{idCaso}")
+    public ResponseEntity<CasoDto> modificarCaso
+    (@PathVariable Long idCaso, @RequestBody ModCasoDto modCasoDto){
+        return ResponseEntity.ok(this.casoService.modificarCaso(idCaso, modCasoDto));
+    }
+
+    //eliminar
+    @DeleteMapping("{idCaso}")
+    public ResponseEntity<Void> eliminarCaso(@PathVariable Long idCaso){
+        this.casoService.eliminarCaso(idCaso);
+        return ResponseEntity.ok().build();
+    }
+}
