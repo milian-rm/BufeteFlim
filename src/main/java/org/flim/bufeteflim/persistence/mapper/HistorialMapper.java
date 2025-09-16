c121f70befc1ef7d9601c974d123f5302df35048
DevJosL <jmoran-2022388@kinal.edu.gt>
2025-09-16 11:34:15 -0600

    T23/ Se realizo el dto, Mapping y entity de la entidad Historial

---
+++ src/main/java/org/flim/bufeteflim/persistence/mapper/HistorialMapper.java
@@ -0,0 +1,28 @@
+package org.flim.bufeteflim.persistence.mapper;
+
+import org.flim.bufeteflim.dominio.dto.HistorialDto;
+import org.flim.bufeteflim.dominio.dto.ModHistorialDto;
+import org.flim.bufeteflim.persistence.entity.HistorialEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface HistorialMapper {
+
+
+    @Mapping(source = "descripcion", target = "description")
+    @Mapping(source = "idAbogado", target = "idLawyer")
+    @Mapping(source = "idCaso", target = "idCase")
+    HistorialDto toDto(HistorialEntity entity);
+
+    List<HistorialDto> toDto(Iterable<HistorialEntity> entities);
+
+    @InheritInverseConfiguration
+    HistorialDto toEntity(HistorialDto dto);
+
+    void modificarEntityFromDto(ModHistorialDto mod, @MappingTarget HistorialEntity entity);
+}
