97bde01a5ea483c81529ffb1dd1f57abbf726bf5
Lendrock <lchoxon-2021286@kinal.edu.gt>
2025-09-17 09:43:40 -0600

    T30/ Se creó EntityRepository de la entidad DetalleCasos

---
+++ src/main/java/org/flim/bufeteflim/persistence/DetalleCasoEntityRepository.java
@@ -0,0 +1,71 @@
+package org.flim.bufeteflim.persistence;
+
+import org.flim.bufeteflim.dominio.dto.DetalleCasoDto;
+import org.flim.bufeteflim.dominio.dto.ModDetalleCasoDto;
+import org.flim.bufeteflim.dominio.exception.DetalleCasoNoExisteException;
+import org.flim.bufeteflim.dominio.exception.DetalleCasoYaExisteException;
+import org.flim.bufeteflim.dominio.repository.DetalleCasoRepository;
+import org.flim.bufeteflim.persistence.crud.CrudDetalleCasoEntity;
+import org.flim.bufeteflim.persistence.entity.DetalleCasoEntity;
+import org.flim.bufeteflim.persistence.mapper.DetalleCasoMapper;
+
+import java.util.List;
+
+public class DetalleCasoEntityRepository implements DetalleCasoRepository {
+
+    private final CrudDetalleCasoEntity crudDetalleCasoEntity;
+    private final DetalleCasoMapper detalleCasoMapper;
+
+    public DetalleCasoEntityRepository
+            (CrudDetalleCasoEntity crudDetalleCasoEntity, DetalleCasoMapper detalleCasoMapper){
+        this.crudDetalleCasoEntity = crudDetalleCasoEntity;
+        this.detalleCasoMapper = detalleCasoMapper;
+    }
+
+    @Override
+    public List<DetalleCasoDto> obtenertodo() {
+        return this.detalleCasoMapper.toDto(this.crudDetalleCasoEntity.findAll());
+    }
+
+    @Override
+    public DetalleCasoDto buscarPorCodigo(Long codeDetail) {
+        return this.detalleCasoMapper.toDto(this.crudDetalleCasoEntity.findById(codeDetail).orElse(null));
+    }
+
+    @Override
+    public DetalleCasoDto guardarDetalleCaso(DetalleCasoDto detalleCasoDto) {
+        if (this.crudDetalleCasoEntity.findFirstById(detalleCasoDto.codeDetail()) != null){
+            throw new DetalleCasoYaExisteException(detalleCasoDto.codeDetail());
+        }
+        DetalleCasoEntity detalleCaso = new DetalleCasoEntity();
+
+        detalleCaso = this.detalleCasoMapper.toEntity(detalleCasoDto);
+
+        this.crudDetalleCasoEntity.save(detalleCaso);
+
+        return  this.detalleCasoMapper.toDto(detalleCaso);
+    }
+
+    @Override
+    public DetalleCasoDto modificarDetalleCaso(Long codeDetail, ModDetalleCasoDto modDetalleCasoDto) {
+        DetalleCasoEntity detallecaso = this.crudDetalleCasoEntity.findById(codeDetail).orElse(null);
+
+        if (detallecaso == null){
+            throw new DetalleCasoNoExisteException(codeDetail);
+        }
+        this.detalleCasoMapper.modificarEntityFromDto(modDetalleCasoDto, detallecaso);
+
+        return this.detalleCasoMapper.toDto(this.crudDetalleCasoEntity.save(detallecaso));
+    }
+
+    @Override
+    public void eliminarDetalleCaso(Long codeDetail) {
+        DetalleCasoEntity detalleCaso = this.crudDetalleCasoEntity.findById(codeDetail).orElse(null);
+
+        if (detalleCaso == null) {
+            throw new DetalleCasoNoExisteException(codeDetail);
+        }else{
+            this.crudDetalleCasoEntity.deleteById(codeDetail);
+        }
+    }
+}
--- src/main/java/org/flim/bufeteflim/persistence/crud/CrudDetalleCasoEntity.java
+++ src/main/java/org/flim/bufeteflim/persistence/crud/CrudDetalleCasoEntity.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.flim.bufeteflim.persistence.entity.*;
 
 public interface CrudDetalleCasoEntity extends CrudRepository<DetalleCasoEntity, Long> {
+    DetalleCasoEntity findFirstById(Long codeDetail);
 }
