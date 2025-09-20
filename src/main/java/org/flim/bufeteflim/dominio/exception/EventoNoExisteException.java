1efa660dc9e70b9741fa3a89aaa2dc68f3cfec10
milian-rm <rmilian-2021493@kinal.edu.gt>
2025-09-20 17:10:34 -0600

    T42/ Se crearon las Validaciones y Excepciones para Eventos

--- src/main/java/org/flim/bufeteflim/dominio/exception/EventoNoExisteException.java
+++ src/main/java/org/flim/bufeteflim/dominio/exception/EventoNoExisteException.java
@@ -2,6 +2,6 @@ package org.flim.bufeteflim.dominio.exception;
 
 public class EventoNoExisteException extends RuntimeException {
     public EventoNoExisteException(Long codigo) {
-        super("El evento con código: "+codigo+" no existe en el sistema");
+        super("El evento con el código: "+codigo+" no existe en el sistema");
     }
 }
--- src/main/java/org/flim/bufeteflim/dominio/exception/EventoYaExisteException.java
+++ src/main/java/org/flim/bufeteflim/dominio/exception/EventoYaExisteException.java
@@ -2,6 +2,6 @@ package org.flim.bufeteflim.dominio.exception;
 
 public class EventoYaExisteException extends RuntimeException {
     public EventoYaExisteException(Long codigo) {
-        super("El evento con el código: "+codigo+" ya existe");
+        super("El evento con código: "+codigo+" ya existe");
     }
 }
