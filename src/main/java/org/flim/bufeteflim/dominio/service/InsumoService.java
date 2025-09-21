fa972c0695d15b6fd9b555bb72fd70e3568e7e51
milian-rm <rmilian-2021493@kinal.edu.gt>
2025-09-20 18:10:48 -0600

    T54/ Se creó CrudEntityRepository, Service y Repository de la entidad Insumos

---
+++ src/main/java/org/flim/bufeteflim/dominio/service/InsumoService.java
@@ -0,0 +1,37 @@
+package org.flim.bufeteflim.dominio.service;
+
+import org.flim.bufeteflim.dominio.dto.InsumoDto;
+import org.flim.bufeteflim.dominio.dto.ModInsumoDto;
+import org.flim.bufeteflim.dominio.repository.InsumoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InsumoService {
+    private final InsumoRepository insumoRepository;
+
+    public InsumoService(InsumoRepository insumoRepository) {
+        this.insumoRepository = insumoRepository;
+    }
+
+    public List<InsumoDto> obtenerTodo() {
+        return this.insumoRepository.obtenerTodo();
+    }
+
+    public InsumoDto buscarPorCodigo(Long id){
+        return this.insumoRepository.buscarPorCodigo(id);
+    }
+
+    public InsumoDto guardarInsumo(InsumoDto insumoDto) {
+        return this.insumoRepository.guardarInsumo(insumoDto);
+    }
+
+    public InsumoDto modificarInsumo(Long id, ModInsumoDto modInsumoDto) {
+        return this.insumoRepository.modificarInsumo(id, modInsumoDto);
+    }
+
+    public void eliminarInsumo(Long id) {
+        this.insumoRepository.eliminarInsumo(id);
+    }
+}
